8e3ff57420b13062383c49d43ff2ace5b34f3ae9
Z3R0th-13 <z3r0th2431@gmail.com>
2020-05-19 11:09:42 -0400

    Add files via upload

---
+++ Aware/officeEnum.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.Win32;
+
+namespace Aware
+{
+    public class officeEnum
+    {
+        public static void OfficeEnum()
+        {
+            try
+            {
+                RegistryKey checkme = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Office\\16.0\\PowerPoint\\Security\\Trusted Documents"); // Hive and keys to look at
+
+                object trustRecs = checkme.GetValue("TrustRecords", string.Empty); // Subkey to actually look at
+
+                if (trustRecs != null)
+                {
+                    using (RegistryKey tempKey = checkme.OpenSubKey("TrustRecords"))
+                    {
+                        Console.WriteLine("\r\n=== PPT TRUSTED FILES ===\r\n");
+                        foreach (string valueName in tempKey.GetValueNames()) // For every value in the subkey, print the values.
+                        {
+                            Console.WriteLine("\t" + "[*] " + "{0}: {1}", valueName, tempKey.GetValue(valueName).ToString());
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\r\n=== PPT TRUSTED FILES ===\r\n");
+                    Console.WriteLine("\t" + "[*] " + "I couldn't find anything");
+                    Console.WriteLine(trustRecs);
+                }
+            }
+            catch
+            {
+                // Do nothing
+            }
+
+            try
+            {
+                RegistryKey checkme = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Office\\16.0\\Excel\\Security\\Trusted Documents"); // Hive and keys to look at
+                object trustRecs = checkme.GetValue("TrustRecords", string.Empty); // Subkey to actually look at
+
+                if (trustRecs != null)
+                {
+                    using (RegistryKey tempKey = checkme.OpenSubKey("TrustRecords"))
+                    {
+                        Console.WriteLine("\r\n=== EXCEL TRUSTED FILES ===\r\n");
+                        foreach (string valueName in tempKey.GetValueNames()) // For every value in the subkey, print the values.
+                        {
+                            Console.WriteLine("\t" + "[*] " + "{0}: {1}", valueName, tempKey.GetValue(valueName).ToString());
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\r\n=== EXCEL TRUSTED FILES ===\r\n");
+                    Console.WriteLine("\t" + "[*] " + "I couldn't find anything");
+                    Console.WriteLine(trustRecs);
+                }
+            }
+            catch
+            {
+                // Do nothing
+            }
+
+            try
+            {
+                RegistryKey checkme = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Office\\16.0\\Word\\Security\\Trusted Documents"); // Hive and keys to look at
+                object trustRecs = checkme.GetValue("TrustRecords", string.Empty); // Subkey to actually look at 
+
+                if (trustRecs != null)
+                {
+                    using (RegistryKey tempKey = checkme.OpenSubKey("TrustRecords"))
+                    {
+                        Console.WriteLine("\r\n=== WORD TRUSTED FILES ===\r\n");
+                        foreach (string valueName in tempKey.GetValueNames()) // For every value in the subkey, print the values
+                        {
+                            Console.WriteLine("\t" + "[*] " + "{0}: {1}", valueName, tempKey.GetValue(valueName).ToString());
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\r\n=== WORD TRUSTED FILES ===\r\n");
+                    Console.WriteLine("\t" + "[*] " + "I couldn't find anything");
+                    Console.WriteLine(trustRecs);
+                }
+            }
+            catch
+            {
+                // Do nothing
+            }
+        }
+
+    }
+}
---
+++ Aware/queryMcAfee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Win32;
+
+namespace Aware
+{
+    public class queryMcAfee
+    {
+        public static void QueryMcafee() // Will query the McAfee registry key for exclusions. 
+        {
+            // NEED TO FIX THIS! I NEED THE REGISTRY PATH AND KEY VALUE FOR MCAFEE!
+            RegistryKey exclusion = Registry.LocalMachine.OpenSubKey(@"HKEY_LOCAL_MACHINE\SOFTWARE\McAfee\Endpoint\EXCLUSION");
+            if (exclusion != null)
+            {
+                Console.WriteLine(exclusion.GetValue("setting"));
+            }
+            else
+            {
+                // Do nothing
+            }
+        }
+    }
+}
